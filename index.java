c2c2cfb6b5ab9d61310f733ccdfda37555df075c
Sakthi-S29 <sakthisharanm@gmail.com>
2024-10-19 13:12:08 -0300

    Added name to the print statement

--- index.java
+++ index.java
@@ -1,7 +1,10 @@
 import java.util.*;
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in());
+		String name = scanner.nextLine();
+		System.out.println("Hello " + name);
 	}
+
 }
 
