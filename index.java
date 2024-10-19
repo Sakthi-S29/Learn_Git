16101ceda2c58c3c493ff656e890de23dff282fd
Sakthi-S29 <sakthisharanm@gmail.com>
2024-10-19 13:49:03 -0300

    asking how are you

--- index.java
+++ index.java
@@ -4,6 +4,7 @@ public class Main{
 		Scanner scanner = new Scanner(System.in());
 		String name = scanner.nextLine();
 		System.out.println("Hello " + name);
+		System.out.println("How are you " + name "?");
 	}
 
 }
